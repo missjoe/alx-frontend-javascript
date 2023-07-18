6450fe044d73ff36eb3a39221d9cd6d8e146ff0a
John Tochi Josephine <johntochijosephine1995@gmail>
2023-07-18 10:00:58 +0000

    Task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,12 @@
+// 7-getBudgetObject.js
+export default function getBudgetObject(income, gdp, capita) {
+  // Use the shorthand property syntax to create the object
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
+
